090429ea0a2a3bddc9a21e05411a4122b41fc426
907888614@qq.com <616483154>
2017-12-29 19:51:32 +0800

    common

--- src/main/java/org/font/DemoApplication.java
+++ src/main/java/org/font/DemoApplication.java
@@ -3,9 +3,13 @@ package org.font;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan
+@EnableScheduling
+@EnableAsync
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/test/java/org/font/task/TaskTest.java
@@ -0,0 +1,45 @@
+package org.font.task;
+
+import org.font.DemoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.concurrent.Future;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {MockServletContext.class, DemoApplication.class})
+@WebAppConfiguration
+public class TaskTest {
+
+    @Autowired
+    private AsyncTask asyncTask;
+
+    @Test
+    public void test() throws Exception {
+
+        long start = System.currentTimeMillis();
+
+        Future<String> task1 = asyncTask.doTaskOne();
+        Future<String> task2 = asyncTask.doTaskTwo();
+        Future<String> task3 = asyncTask.doTaskThree();
+
+        while (true) {
+            if (task1.isDone() && task2.isDone() && task3.isDone()) {
+                // 三个任务都调用完成，退出循环等待
+                break;
+            }
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
+
+    }
+
+
+}
